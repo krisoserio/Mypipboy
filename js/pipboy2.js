636cbb7c4f34a0de5157d493c299349ebf43349c
krisoserio <khristianoserio50@gmail.com>
2020-12-04 13:10:24 +0800

    pipboy

--- js/pipboy2.js
+++ js/pipboy2.js
@@ -3,61 +3,78 @@ $(document).ready(function(){
   var weapons = [
     {"name": "pistol",
     "damage": "30",
-    "damage-second": "124.2",
+    "second": "124.2",
     "fire-rate": "46",
     "magazine": "24",
     "accuracy": "90%",
     "weight": "4.2"
   },
-  {"name": "The-Gainer",
+  {"name": "Gainer",
   "damage": "48",
-  "damage-second": "28.8",
-  "fire-rate": "6",
+  "second": "28.8",
+  "rate": "6",
   "magazine": "6",
   "accuracy": "70%",
   "weight": "4.27"
 },
-{"name": "Assault-rifle",
+{"name": "rifle",
 "damage": "30",
-"damage-second": "120",
-"fire-rate": "40",
+"second": "120",
+"rate": "40",
 "magazine": "30",
 "accuracy": "60%",
 "weight": "7.2"
 },
+{"name": "gen",
+"damage": "32",
+"second": "128",
+"fire-rate": "40",
+"magazine": "66",
+"accuracy": "100%",
+"weight": "7"
+},
 {"name": "last",
-"damage": "30",
-"damage-second": "124.2",
-"fire-rate": "46",
-"magazine": "24",
-"accuracy": "90%",
-"weight": "4.2"
+"damage": "192",
+"second": "200",
+"rate": "46",
+"magazine": "66",
+"accuracy": "112%",
+"weight": "12"
 },
 {"name": "pist",
 "damage": "57",
-"damage-second": "56",
-"fire-rate": "46",
+"second": "56",
+"fire-rate": "59",
 "magazine": "24",
 "accuracy": "90%",
-"weight": "4.2"
+"weight": "7"
 },
 {"name": "rare",
 "damage": "51",
 "damage-second": "576.3",
-"fire-rate": "113",
+"rate": "113",
 "magazine": "75",
 "accuracy": "108%",
 "weight": "10"
 },
 {"name": "pi",
 "damage": "37",
-"damage-second": "11.2",
-"fire-rate": "3",
+"second": "11.2",
+"rate": "3",
 "magazine": "5",
 "accuracy": "71%",
 "weight": "13.2"
 }
   ]
+$('.item-list a').on('click',function(e){
+  $('.item-list a').removeClass('active');
+  $(e.currentTarget).addClass('active');
+});
+$('.item-list li').on('click',function(e){
+  $('.item-list li').removeClass('active');
+  $(e.currentTarget).addClass('active');
+});
+
 $('.item-list a').on('mouseenter',function(e){
   var current_item = $(e.currentTarget).attr('class');
   console.log(current_item);
@@ -68,6 +85,11 @@ $('.item-list a').on('mouseenter',function(e){
 
       var container= $('.item-stats');
       container.find('.damage').html(weapons[item].damage);
+      container.find('.second').html(weapons[item].second);
+      container.find('.rate').html(weapons[item].rate);
+      container.find('.magazine').html(weapons[item].magazine);
+      container.find('.accuracy').html(weapons[item].accuracy);
+      container.find('.weight').html(weapons[item].weight);
     }
   };
 });
